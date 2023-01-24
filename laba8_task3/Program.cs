996c60ba096d3ddf1b7393ba791f0d9346ac78c1
irinacreek <irinacreek2@gmailcom>
2023-01-25 03:54:10 +0500

    3

---
+++ laba8_task3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = new string[] { "code", "doce", "ecod", "framer", "frame" };
+            string[] changedArray = DeleteAnagrams(array);
+            foreach (string s in changedArray)
+                Console.WriteLine(s);
+        }
+
+        private static string[] DeleteAnagrams(string[] array)
+        {
+            List<string> list = new()
+            {
+                array[0]
+            };
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (!IsAnagram(array[i], array[i - 1]))
+                    list.Add(array[i]);
+            }
+
+            list.Sort();
+            return list.ToArray();
+        }
+
+        private static bool IsAnagram(string word1, string word2)
+        {
+            bool result = true;
+            char[] chars1 = word1.ToCharArray();
+            char[] chars2 = word2.ToCharArray();
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+            if (chars1.Length != chars2.Length)
+                return false;
+            else
+            {
+                for (int i = 0; i < chars1.Length; i++)
+                {
+                    if (chars1[i] != chars2[i])
+                        result = false;
+                }
+            }
+            return result;
+        }
+    }
+}
